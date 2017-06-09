ca3fa02e68cc195906a66a1652f8858889bd54c6
qingw <qingw@jumei.com>
2017-06-09 17:04:48 +0800

    1. 添加 CardView 测试。

--- app/src/main/java/com/zkteam/aoc/MainFragment.java
+++ app/src/main/java/com/zkteam/aoc/MainFragment.java
@@ -1,7 +1,9 @@
 package com.zkteam.aoc;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,5 +21,16 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        CardView mCardView = (CardView) view.findViewById(R.id.cardView);
+
+//        mCardView.setRadius(8);//设置图片圆角的半径大小
+//        mCardView.setCardElevation(8);//设置阴影部分大小
+//        mCardView.setContentPadding(5,5,5,5);//设置图片距离阴影大小
+
     }
 }
