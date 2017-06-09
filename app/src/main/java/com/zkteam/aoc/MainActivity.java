d3abceae0b4bc588dd6321609af5269f89e8b0cd
qingw <qingw@jumei.com>
2017-06-09 14:22:24 +0800

    1. 修改主页样式。

--- app/src/main/java/com/zkteam/aoc/MainActivity.java
+++ app/src/main/java/com/zkteam/aoc/MainActivity.java
@@ -5,10 +5,12 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout container;
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -19,12 +21,15 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
+                    container.setBackgroundResource(R.color.colorAccent);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
+                    container.setBackgroundResource(R.color.colorPrimary);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
+                    container.setBackgroundResource(R.color.colorPrimaryDark);
                     return true;
             }
             return false;
@@ -37,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        container = (RelativeLayout) findViewById(R.id.container);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
